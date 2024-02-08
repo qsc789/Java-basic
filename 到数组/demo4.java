f2cc38e8aab9aa3061b69fe955b31484ab18c812
qsc789 <1279495882@qq.com>
2024-02-08 20:51:02 +0800

    声明数组

---
+++ 到数组/demo4.java
@@ -0,0 +1,12 @@
+package com.itheima.demo1;
+
+public class demo4 {
+    public static void main(String[] args) {
+        int[] arr=new int[]{11,22,33};
+        int[] b={1,2,3};
+        //动态初始化
+        int[] c=new int[10];
+        int[] num =new int[20];
+        int n=arr.length;
+    }
+}
