f07270b9c9a620b6d8888727891e14f166ff30c1
qsc789 <1279495882@qq.com>
2024-02-09 14:22:42 +0800

    不换行打印

---
+++ 方法/MethodDemo2.java
@@ -0,0 +1,17 @@
+package com.itheima.methoddemo;
+
+public class MethodDemo2 {
+    public static void main(String[] args) {
+        //遍历数组
+        int[] arr={11,22,33,44,55};
+        PrintArr(arr);
+    }
+
+    public static void PrintArr(int[] arr) {
+        //arr.fori
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+",");//只打印不换行
+        }
+
+    }
+}
